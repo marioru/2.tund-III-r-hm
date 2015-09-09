990ac5fdcfd98240cb160d1e19d7d740e08e8a15
Your Name <you@example.com>
2015-09-09 11:39:46 +0300

    salvesta file

--- tere.php
+++ tere.php
@@ -1,1 +1,23 @@
-hello
+<?php
+	$first_name= "Marianne";
+	$last_name= "Orusalu";
+	
+	echo $first_name." ".$last_name;
+?>
+<br>
+<?php
+
+	$age = 5;
+	
+	//testib loogikat,juhul kui vanus on väiksem 
+	//kui 18 siis kirjuta "alaealine", muul juhul "täisealine"
+	
+	//if'i sisse loogikatehe
+	if($age <18) {
+		//tõde
+		echo "alaealine";
+	} else {
+		//väär
+		echo "täisealine"
+	}
+?>
